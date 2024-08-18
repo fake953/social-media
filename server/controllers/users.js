6bec539d64a1f6060f3779a6621aa8fecae10945
osman <osmankain85@gmail.com>
2024-08-18 12:53:04 +0430

    get user endpoint added

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,18 @@
-import User from "../model/user";
+import User from "../model/user.js";
 
-export const getUser =(req,res) =>{
-    const {id} = res.status.jso
-}
+export const getUser = async (req, res) => {
+  try {
+    const { id } = res.params;
+    const user = await User.findById(id);
+
+    res.status(200).json({
+      data: user,
+      message: "ok",
+    });
+  } catch (error) {
+    res.status(400).json({
+      data: null,
+      message: error.message,
+    });
+  }
+};
