754572fbc62b301340350af101e4e843c2ea4ef7
osman <osmankain85@gmail.com>
2024-09-10 12:38:40 +0430

    update project

--- client/src/services/api/apiQuery.ts
+++ client/src/services/api/apiQuery.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiQuery = createApi({
   reducerPath: "apiQuery",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://social-media-sandy-five.vercel.app/",
+    // baseUrl: "https://social-media-sandy-five.vercel.app",
+    baseUrl: "http://localhost:5000",
   }),
   endpoints: (builder) => ({
     //AUTHENTICATION ENDPOINTS
@@ -16,7 +17,7 @@ export const apiQuery = createApi({
     }),
     loginUsr: builder.mutation({
       query: (data) => ({
-        url: "auth/login",
+        url: "/auth/login",
         method: "POST",
         body: data,
       }),
--- client/src/utils/getImageAddress.ts
+++ client/src/utils/getImageAddress.ts
@@ -1,2 +1,2 @@
 export const getImageAddress = (imagePath: string) =>
-  `https://social-media-sandy-five.vercel.app/assets/${imagePath}`;
+  `http://localhost:5000/assets/${imagePath}`;
